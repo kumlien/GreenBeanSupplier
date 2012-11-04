fd674c3001b1a2f49591ac1d6e49088c77825427
Svante Kumlien <svante.kumlien@gmail.com>
2012-11-04 16:26:05 +0100

    Added stuff to readme, fixed logging.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ GreenBeanSupplier
 =================
 
 Repo for the Supplier part of the Green Bean lab
+
+This project acts as a supplier in the lab. It accepts new orders via ftp. The ftp-server is configured in the Spring context file and in a properties file (users.properties)
--- src/main/java/se/ithuset/greenbean/supplier/Main.java
+++ src/main/java/se/ithuset/greenbean/supplier/Main.java
@@ -2,7 +2,7 @@ package se.ithuset.greenbean.supplier;
 
 import org.apache.ftpserver.FtpServer;
 import org.apache.ftpserver.ftplet.FtpException;
-import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -13,15 +13,13 @@ public class Main {
 	 * @throws FtpException 
 	 */
 	public static void main(String[] args) throws FtpException {
-        BasicConfigurator.configure();
-
+        final Logger logger = Logger.getLogger(Main.class);
+        logger.info("The supplier system is starting up...");
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("META-INF/spring/app-context.xml");
-		String[] beanNames = ctx.getBeanDefinitionNames();
-		for(String beanName : beanNames) {
-			System.out.println(beanName + " of type " + ctx.getBean(beanName).getClass().getName());
-		}
+		logger.info("Spring context loaded");
 		
 		FtpServer server = ctx.getBean(FtpServer.class);
+		logger.info("Ftp server fetched from context");
 		server.start();
 	}
 
