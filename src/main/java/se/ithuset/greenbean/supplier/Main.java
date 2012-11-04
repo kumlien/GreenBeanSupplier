8ddb58a0e6bf4f94d708c34d58e5effde6ccdbf6
Per-Ingemar Andersson <peringemar@gmail.com>
2012-11-04 15:34:40 +0100

    Finlir

--- src/main/java/se/ithuset/greenbean/supplier/Main.java
+++ src/main/java/se/ithuset/greenbean/supplier/Main.java
@@ -2,6 +2,7 @@ package se.ithuset.greenbean.supplier;
 
 import org.apache.ftpserver.FtpServer;
 import org.apache.ftpserver.ftplet.FtpException;
+import org.apache.log4j.BasicConfigurator;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,6 +13,8 @@ public class Main {
 	 * @throws FtpException 
 	 */
 	public static void main(String[] args) throws FtpException {
+        BasicConfigurator.configure();
+
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("META-INF/spring/app-context.xml");
 		String[] beanNames = ctx.getBeanDefinitionNames();
 		for(String beanName : beanNames) {
