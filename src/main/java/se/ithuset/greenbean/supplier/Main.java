ce84db861cb9ea5379e0a544c7db1cd656fa2ef0
Svante Kumlien <svante.kumlien@gmail.com>
2012-11-04 15:26:09 +0100

    Inital commit

---
+++ src/main/java/se/ithuset/greenbean/supplier/Main.java
@@ -0,0 +1,25 @@
+package se.ithuset.greenbean.supplier;
+
+import org.apache.ftpserver.FtpServer;
+import org.apache.ftpserver.ftplet.FtpException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws FtpException 
+	 */
+	public static void main(String[] args) throws FtpException {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("META-INF/spring/app-context.xml");
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		for(String beanName : beanNames) {
+			System.out.println(beanName + " of type " + ctx.getBean(beanName).getClass().getName());
+		}
+		
+		FtpServer server = ctx.getBean(FtpServer.class);
+		server.start();
+	}
+
+}
